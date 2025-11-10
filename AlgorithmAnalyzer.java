61f2bd7199944c5d92bd99865245d967b97d1077
DasunHW <119805511+DasunHW@users.noreply.github.com>
2025-11-10 16:20:05 +0530

    Update AlgorithmAnalyzer.java
    Member 3 - Bubble Sort

--- AlgorithmAnalyzer.java
+++ AlgorithmAnalyzer.java
@@ -78,5 +78,42 @@ public class AlgorithmAnalyzer {
             }
         }
     }
+
+    // Member 3: Bubble Sort Implementation
+    public static class BubbleSortAnalyzer {
+        public static void bubbleSort(int[] arr) {
+            int n = arr.length;
+            for (int i = 0; i < n - 1; i++) {
+                for (int j = 0; j < n - i - 1; j++) {
+                    if (arr[j] > arr[j + 1]) {
+                        // Swap
+                        int temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                    }
+                }
+            }
+        }
+        
+        public static void analyzeBubbleSort() {
+            System.out.println("\n=== Member 3: Bubble Sort ===");
+            System.out.println("Algorithm: Bubble Sort");
+            System.out.println("Input Size | Time (ms)");
+            System.out.println("----------------------");
+            
+            int[] sizes = {100, 500, 1000};
+            
+            for (int size : sizes) {
+                int[] arr = generateRandomArray(size);
+                
+                long startTime = System.nanoTime();
+                bubbleSort(arr);
+                long endTime = System.nanoTime();
+                
+                double timeMs = (endTime - startTime) / 1_000_000.0;
+                System.out.printf("%-10d | %.2f\n", size, timeMs);
+            }
+        }
+    }
     
     
